e959481dfaa90dc6f6f0e93c0b046437d376fb22
Dauren-Ayazbayev <Dauren.Ayazbayev@gmail.com>
2021-10-21 15:58:09 +0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,84 +11,132 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
+            *{
+                box-sizing: border-box;
             }
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+       <nav style="position:fixed;" class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+             <img src="ACeSYRI_logo-2-300x75.png" style="width:10%">
+             <img src="2.jpg" style="width:10%;margin-left: 15px;">
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#workshop_information" style="margin-left:200px">Workshop information</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#topics">Topics</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#important_dates">Important dates</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#program_content" >Program content</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#committees" >Committees</a>
+                  </li>
+                </ul>
+              </div>
+              
+            </div>
+          </nav>
+          <a name="workshop_information"></a>
+          <br><br><br>
+        
+        <h2 style="text-align: center;">International Workshop on Presentation Skills for an Academic Topic</h2>
+        <h3 style="text-align: center;">23 November 2021</h3>
+        <div style="width: 95%; display: flex; justify-content:center; margin:auto;">
+            <div>  
+                <p>This international workshop on presentation skills of an academic topic is organized in the frame of ACeSYRI<sup>1</sup> and ICECCO<sup>2</sup>. In this workshop, participants will work on preparing and making a presentation for an academic paper. That will help them to rehearse on their conference presentation to be made in ICECCO’21 as well as on any kind of scientific presentation. The participants will have opportunities to attend the lectures and get feedback for their performances from local and international experts.
+                </p>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                The workshop will be supported by the following projects:
+                <ol>
+                    <li>Advanced Centre for PhD Students and Young Researchers in Informatics (ACeSYRI) (Project EACEA.CBHE no.: 610166-EPP-1-2019-1-SK-EPPKA2-CBHE-JP supported by <a href="https://eacea.ec.europa.eu/erasmus-plus_en">the European Union's Erasmus+ programme</a>)(<a href="https://acesyri.eu/">https://acesyri.eu/</a>). 
+                    </li>
+                    <li>
+                        The 16<sup>th</sup> International Conference on Electronics Computer and Computation ICECCO’21 organized jointly by Suleyman Demirel University, Kazakhstan, Nile University, Nigeria and Alatoo University, Kyrgyzstan (<a href="https://icecco.net/">https://icecco.net/</a>).
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                    </li>
+                </ol>
+                <strong>Because the workshop will be held online, it is necessary to register by using <a href="">this link</a>.</strong>
+                <br><br><br>
+                <h4>Format</h4>
+                <p>The participants are supposed to prepare a presentation with 8-15 slides and submit it until 13<sup>th</sup> November. During the workshop, they will have the opportunity to edit and refine the slides before making the presentation. Each work is going to be evaluated by a committee formed by local and international experts. The successful works will be awarded in different categories such as the best presentation, the best scientific work, etc.
+                </p>
+
+                <a name="topics"></a>
+                <br><br><br>
+                <h4>Topics</h4>
+                <p>The workshop is focused on the same topics as declared in ICECCO site with a wide range of applications of computer systems. Topics of interest include but are not limited to:
+                </p>
+                <ul>
+                    <li>Wireless and mobile sensor networks</li>
+                    <li>Control Engineering</li>
+                    <li>Communication Engineering</li>
+                    <li>Scientific Computation</li>
+                    <li>Modeling and Simulation</li>
+                    <li>Artificial Intelligence</li>
+                    <li>Computer Education</li>
+                    <li>Programming Languages</li>
+                    <li>E-governance</li>
+                    <li>Biomedical Engineering/Biotechnology</li>
+                    <li>Signal Processing</li>
+                    <li>Bioinformatics</li>
+                    <li>Computer Vision</li>
+                    <li>Micro-Electronics Engineering</li>
+                    <li>Cyber Security</li>
+                    <li>Data Science</li>
+                    <li>Mechatronics/Robotics and embedded systems</li>
+                    <li>Interesting Applications/Software Engineering</li>
+                    <li>Gamification</li>
+                    <li>Electromagnetic Engineering</li>
+                    <li>IT Law</li>
+                </ul>
+
+                <a name="important_dates"></a>
+                <br><br><br>
+                <h4>Important Dates</h4>
+                Registration and work submission: <strong>13<sup>th</sup> of November, 2021</strong><br>
+                Workshop day:<strong>23<sup>th</sup> of November,2021</strong>
+                
+                <a name="program_content"></a>
+                <br><br><br>
+                <h4>Program Content</h4>
+                <table class="table table-hover">
+                    <tr><td>09:30</td><td>Opening and keynote speak</td></tr>
+                    <tr><td>10:20</td><td>Introducing ACeSYRI and ICECCO</td></tr>
+                    <tr><td>11:00</td><td>Lecture on “Presentation skills for a scientific work”</td></tr>
+                    <tr><td>12:00</td><td>Lunch time</td></tr>
+                    <tr><td>13:00</td><td>Working on the presentations and refining the slides</td></tr>
+                    <tr><td>14:00</td><td>Individual presentations</td></tr>
+                    <tr><td>17:30</td><td>Award ceremony and closing</td></tr>
+                </table>
+                <a name="committees"></a>
+                <br><br>
+                
+                <h4>Program Committee</h4>
+                <ul>
+                    <li><b>Martin Lukac,</b><i> Nazarbayev University, Nur-Sultan, Kazakhstan</i></li>
+                </ul>
+
+                <br><br>
+                <h4>Organizing Committee</h4>
+                <ul>
+                    <li> <b>Azamat Zhamanov,</b><i> Suleyman Demirel University, Kaskelen, Almaty, Kazakhstan</i></li>
+                    <li><b>Madina Alimanova,</b><i> Suleyman Demirel University, Kaskelen, Almaty, Kazakhstan</i></li>
+                    <li><b>Nazerke Sultanova,</b><i> Suleyman Demirel University, Kaskelen, Almaty, Kazakhstan</i></li>
+                    <li><b>Cemil Turan,</b><i> Suleyman Demirel University, Kaskelen, Almaty, Kazakhstan</i></li>
+                </ul>
             </div>
         </div>
     </body>
